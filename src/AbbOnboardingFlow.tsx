7848ccc3a720decdef690252ac4b0944e6712891
Mymlogic.official <mymlogic.official@gmail.com>
2026-01-22 16:28:31 -0800

    Create AbbOnboardingFlow.tsx
    Signed-off-by: Mymlogic.official <mymlogic.official@gmail.com>

---
+++ src/AbbOnboardingFlow.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AbbOnboardingFlow() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: '20px' }}>
+      <h1>Welcome to ABB Onboarding!</h1>
+      <p>This is your onboarding flow component.</p>
+    </div>
+  );
+}
+ 
