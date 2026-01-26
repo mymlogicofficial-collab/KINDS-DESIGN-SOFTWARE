2762439c400a83f0d0c449ae2255d2f5f3ddc10e
Mymlogic.official <mymlogic.official@gmail.com>
2026-01-25 19:50:01 -0800

    Create hardware_map.h
    Signed-off-by: Mymlogic.official <mymlogic.official@gmail.com>

---
+++ hardware_map.h
@@ -0,0 +1,6 @@
+// SE CUSTOMS - HARDWARE PIN MAPPING
+#define MAGNET_PIN 18      // Output to Electromagnet
+#define TOUCH_SENSOR_L 14  // Left 2x Tap Sensor
+#define TOUCH_SENSOR_R 15  // Right 2x Tap Sensor
+#define DRAWER_ACTUATOR 22 // Main Drawer Release
+#define CABINET_SENSOR 27   // Curios Cabinet Detection
