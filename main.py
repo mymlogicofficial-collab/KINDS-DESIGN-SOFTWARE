278c096d95a9334fe01862f2413b22ba8b2ee177
Mymlogic.official <mymlogic.official@gmail.com>
2026-01-28 19:09:57 -0800

    Update main.py
    Signed-off-by: Mymlogic.official <mymlogic.official@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from kivy.properties import NumericProperty, BooleanProperty
-
 class KindsRoot(BoxLayout):
     active_lead = StringProperty("3H")
     active_soft = StringProperty("2B")
+    active_tip = StringProperty("°") # Starting with the round tip
     
-    # Drawer F Properties
     drawer_f_open = BooleanProperty(False)
     tip_size = NumericProperty(10)
     tip_opacity = NumericProperty(100)
@@ -14,8 +12,10 @@ class KindsRoot(BoxLayout):
         super().__init__(**kwargs)
         self.h_leads = ["3H", "4H", "5H"]
         self.b_leads = ["2B", "4B", "8B"]
+        self.tip_shapes = ["°", "\\", "/", "÷", "L"] # Your specific list
         self.h_index = 0
         self.b_index = 0
+        self.tip_index = 0
 
     def cycle_h_lead(self):
         self.h_index = (self.h_index + 1) % len(self.h_leads)
@@ -25,7 +25,11 @@ class KindsRoot(BoxLayout):
         self.b_index = (self.b_index + 1) % len(self.b_leads)
         self.active_soft = self.b_leads[self.b_index]
 
+    def cycle_tip_shape(self):
+        # Cycles through ° \ / ÷ L
+        self.tip_index = (self.tip_index + 1) % len(self.tip_shapes)
+        self.active_tip = self.tip_shapes[self.tip_index]
+
     def toggle_drawer_f(self):
-        # Tapping F opens/closes the sliders
         self.drawer_f_open = not self.drawer_f_open
         
