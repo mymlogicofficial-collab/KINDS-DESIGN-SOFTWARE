371a317c62080359d6ebb2f091fc71442bab358d
Mymlogic.official <mymlogic.official@gmail.com>
2026-01-28 19:37:38 -0800

    Update main.py
    Signed-off-by: Mymlogic.official <mymlogic.official@gmail.com>

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+from kivy.properties import ListProperty
+
 class KindsRoot(BoxLayout):
     active_lead = StringProperty("3H")
     active_soft = StringProperty("2B")
-    active_tip = StringProperty("°") # Starting with the round tip
+    active_tip = StringProperty("°")
     
+    # Drawer States
     drawer_f_open = BooleanProperty(False)
+    drawer_g_open = BooleanProperty(False) # Added for Color Lab
+    
+    # Drawer F Properties
     tip_size = NumericProperty(10)
     tip_opacity = NumericProperty(100)
     tip_gression = NumericProperty(50)
     
+    # Drawer G Properties (Color Lab)
+    main_hue = NumericProperty(0) # 0 to 360 degrees
+    light_blend = NumericProperty(0) # Slider 1
+    dark_blend = NumericProperty(0)  # Slider 2
+    current_color = ListProperty([1, 1, 1, 1]) # Resulting RGBA
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.h_leads = ["3H", "4H", "5H"]
         self.b_leads = ["2B", "4B", "8B"]
-        self.tip_shapes = ["°", "\\", "/", "÷", "L"] # Your specific list
+        self.tip_shapes = ["°", "\\", "/", "÷", "L"]
         self.h_index = 0
         self.b_index = 0
         self.tip_index = 0
@@ -26,10 +38,13 @@ class KindsRoot(BoxLayout):
         self.active_soft = self.b_leads[self.b_index]
 
     def cycle_tip_shape(self):
-        # Cycles through ° \ / ÷ L
         self.tip_index = (self.tip_index + 1) % len(self.tip_shapes)
         self.active_tip = self.tip_shapes[self.tip_index]
 
     def toggle_drawer_f(self):
         self.drawer_f_open = not self.drawer_f_open
-        
+        self.drawer_g_open = False # Close G if F opens
+
+    def toggle_drawer_g(self): # Added function
+        self.drawer_g_open = not self.drawer_g_open
+        self.drawer_f_open = False # Close F if G opens
